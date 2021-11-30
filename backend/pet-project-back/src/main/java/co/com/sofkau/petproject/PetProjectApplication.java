6b5b4e4804654ebe2650f233c0151d23592104fc
DaironPerilla <perrynikon@gmail.com>
2021-11-30 16:16:59 -0500

    back v1.0.0

---
+++ backend/pet-project-back/src/main/java/co/com/sofkau/petproject/PetProjectApplication.java
@@ -0,0 +1,13 @@
+package co.com.sofkau.petproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PetProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PetProjectApplication.class, args);
+	}
+
+}
---
+++ backend/pet-project-back/src/main/java/co/com/sofkau/petproject/models/Product.java
@@ -0,0 +1,57 @@
+package co.com.sofkau.petproject.models;
+
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.index.Indexed;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+
+@Document(collection = "products")
+public class Product {
+    @Id
+    private String id;
+
+    @Indexed(unique=true)
+    private Long productCode;
+
+    private String name;
+    private double price;
+    private String description;
+
+    public Product(Long productCode, String name, double price) {
+        this.productCode = productCode;
+        this.name = name;
+        this.price = price;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public Long getProductCode() {
+        return productCode;
+    }
+
+    public void setProductCode(Long productCode) {
+        this.productCode = productCode;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+}

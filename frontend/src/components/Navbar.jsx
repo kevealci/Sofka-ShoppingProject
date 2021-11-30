530f669bf8d29af17302e0e446761bff628923ee
DaironPerilla <perrynikon@gmail.com>
2021-11-30 18:14:15 -0500

    nav

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -2,20 +2,20 @@ import React from 'react'
 
 const Navbar = () => {
     return (
-            <ul className="nav nav-pills nav-fill mt-4">
-                <li className="nav-item">
-                    <a className="nav-link active" href="">Active</a>
-                </li>
-                <li className="nav-item">
-                    <a className="nav-link" href="#">Much longer nav link</a>
-                </li>
-                <li className="nav-item">
-                    <a className="nav-link" href="#">Link</a>
-                </li>
-                <li className="nav-item">
-                    <a className="nav-link disabled">Disabled</a>
-                </li>
-            </ul>
+        <ul className="nav nav-pills nav-fill mt-4">
+            <li className="nav-item">
+                <a className="nav-link active" href="">Active</a>
+            </li>
+            <li className="nav-item">
+                <a className="nav-link" href="#">Much longer nav link</a>
+            </li>
+            <li className="nav-item">
+                <a className="nav-link" href="#">Link</a>
+            </li>
+            <li className="nav-item">
+                <a className="nav-link disabled">Disabled</a>
+            </li>
+        </ul>
     )
 }
 

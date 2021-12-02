25c02e6dce8f72cafd7a1f3ddd23c65a6100496b
Yenifer <guajeyenifer@gmail.com>
2021-12-02 17:52:14 -0500

    Se añade la carpeta de backend

---
+++ backend/petproject/src/main/java/co/com/sofkau/petproject/petproject/controller/OrdersController.java
@@ -0,0 +1,51 @@
+package co.com.sofkau.petproject.petproject.controller;
+
+
+import co.com.sofkau.petproject.petproject.models.Orders;
+import co.com.sofkau.petproject.petproject.service.OrderService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+/**
+ * Clase con las metodos del controlador de Ordenes
+ */
+@RestController
+@CrossOrigin(origins = "*")
+@RequestMapping("api/order")
+public class OrdersController {
+
+    @Autowired
+    OrderService orderService;
+
+    @GetMapping
+    public ResponseEntity<List<Orders>> getAll() {
+        return orderService.getAllOrders();
+    }
+
+    @GetMapping(value = "/code/{code}")
+    public ResponseEntity<Orders> getByCode(@PathVariable("code") String code) {
+        return orderService.getByCodeOrder(code);
+    }
+    @GetMapping(value = "/user/{user}")
+    public ResponseEntity<List<Orders>> getByUserName(@PathVariable("user") String user) {
+        return orderService.getByUserName(user);
+    }
+
+    @PostMapping
+    public ResponseEntity<Orders> save(@RequestBody Orders order){
+        return orderService.save(order);
+    }
+
+    @PutMapping(value = "/{code}")
+    public boolean updateByCodeOrder(@PathVariable("code") String code, @RequestBody Orders order){
+        return orderService.updateByCodeOrder(code, order);
+    }
+
+    @DeleteMapping(value = "/{code}")
+    public boolean deleteByCodeOrder(@PathVariable("code") String code){
+        return orderService.deleteByCodeOrder(code);
+    }
+}
